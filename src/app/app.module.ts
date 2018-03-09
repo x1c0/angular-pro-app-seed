029453d49356e6f0eea7c0fabdee20a053e57da7
Francisco Silva <francisco.silva@oddeven.ch>
2018-03-09 09:44:15 +0100

    initial firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,3 +30,14 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+var config = {
+    apiKey: "AIzaSyA0ZOOmUZC_O5MP7MMwo_NtH-QoNC5ak7o",
+    authDomain: "fitness-app-11175.firebaseapp.com",
+    databaseURL: "https://fitness-app-11175.firebaseio.com",
+    projectId: "fitness-app-11175",
+    storageBucket: "fitness-app-11175.appspot.com",
+    messagingSenderId: "414306288153"
+  };
+*/
